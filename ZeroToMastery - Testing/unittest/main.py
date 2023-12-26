2a293caef4cc73c821a0a6fa082d06aa166e46f5
felipecavalcanti5 <2022211510016@iesp.edu.br>
2023-12-26 20:56:19 -0300

    upadte 16 & 17

--- ZeroToMastery - Testing/unittest/main.py
+++ ZeroToMastery - Testing/unittest/main.py
@@ -1,5 +1,8 @@
 def do_stuff(num):
     try:
-        return int(num) + 5
+        if num:
+            return int(num) + 5
+        else:
+            return 'please enter number'
     except ValueError as err:
         return err
--- ZeroToMastery - Testing/unittest/test.py
+++ ZeroToMastery - Testing/unittest/test.py
@@ -2,6 +2,9 @@ import unittest
 import main
 
 class TestMain(unittest.TestCase):
+    def setUp(self): # = BeforeEach
+        print('about to test a function')
+
     def test_do_stuff(self):
         test_param = 10
         result = main.do_stuff(test_param)
@@ -10,7 +13,20 @@ class TestMain(unittest.TestCase):
     def test_do_stuff2(self):
         test_param = "rewqfds"
         result = main.do_stuff(test_param)
-        self.assertTrue(isinstance(result, ValueError))
+        self.assertIsInstance(result, ValueError)
+    
+    def test_do_stuff3(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please enter number')
+
+    def test_do_stuff4(self):
+        test_param = ''
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please enter number')
+
+    def tearDown(self):
+        print('cleaning up') # = AfterEach
 
 
 
